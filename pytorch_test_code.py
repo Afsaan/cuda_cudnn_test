a821ac1e309c5232e69a9bb2391b4490965336ee
“Afsaan” <khanafsaan11@gmail.com>
2021-04-12 10:54:38 +0530

    pytorch file to test cuda and cudnn

---
+++ pytorch_test_code.py
@@ -0,0 +1,13 @@
+import torch
+
+
+print(f'[INFO] Is cuda available -  {torch.cuda.is_available()}')
+
+
+print(f'[INFO] device is -  {torch.cuda.is_available()}')
+
+
+print(f'[INFO] current device name is -  {torch.cuda.get_device_name()}')
+
+
+
